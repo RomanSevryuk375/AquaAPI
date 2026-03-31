67115c156fc0bfe6dc7e0c3c75f8c843734fb901
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-03-31 16:24:33 +0300

    feat(domain): add repository and unit-of-work contracts

---
+++ src/Services/TelemetryService/Telemetry.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Telemetry.Domain.Interfaces;
+
+public interface IRepository<T> where T : class, IEntity
+{
+    Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? filter,
+        int? skip,
+        int? take,
+        CancellationToken cancellationToken = default);
+
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+    void Update(T entity);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
