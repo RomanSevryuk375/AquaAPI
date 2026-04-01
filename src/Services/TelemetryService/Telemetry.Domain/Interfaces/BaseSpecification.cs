f625869ed93cc58b76ed0b63f5bc9f877413fbe7
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-04-01 16:58:29 +0300

    feat(telemetry/domain) implement specification pattern

---
+++ src/Services/TelemetryService/Telemetry.Domain/Interfaces/BaseSpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Telemetry.Domain.Interfaces;
+
+public abstract class BaseSpecification<T>
+{
+    public Expression<Func<T, bool>> Criteria { get; }
+
+    protected BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+}
---
+++ src/Services/TelemetryService/Telemetry.Domain/Specifications/TelemetryFilterSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Telemetry.Domain.Entities;
+using Telemetry.Domain.Interfaces;
+
+namespace Telemetry.Domain.Specifications;
+
+public class TelemetryFilterSpecification : BaseSpecification<TelemetryDataEntity>
+{
+    public TelemetryFilterSpecification(Guid? sensorId, DateTime? from, DateTime? to)
+        : base (data =>
+            (!sensorId.HasValue || data.SensorId == sensorId) &&
+            (!from.HasValue || data.RecordedAt >= from.Value) &&
+            (!to.HasValue || data.RecordedAt <= to.Value))
+    {
+    }
+}
