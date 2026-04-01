90f3d236f767e4bc968dd1b50b9436a7d2d588d0
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-04-01 17:00:24 +0300

    feat(telemetry/application) implement dtos for communication between layers

---
+++ src/Services/TelemetryService/Telemetry.Application/DTOs/TelemetryDataFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Telemetry.Application.DTOs;
+
+public record TelemetryDataFilterDto
+{
+    public Guid? SensorId { get; init; }
+    public DateTime? From { get; init; }
+    public DateTime? To { get; init; }
+}
