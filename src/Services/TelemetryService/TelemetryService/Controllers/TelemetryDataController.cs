ac78b45885517a4444316b2f9092561408297f27
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-04-02 18:29:05 +0300

    fix(telemetry/API) fix routing

---
+++ src/Services/TelemetryService/TelemetryService/Controllers/TelemetryDataController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telemetry.Application.DTOs;
+using Telemetry.Application.Interfaces;
+
+namespace Telemetry.API.Controllers;
+
+[ApiController]
+[Route("api/telemetry/v1/data")]
+public class TelemetryDataController(ITelemetryDataService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<TelemetryDataResponse>>> GetAllDataAsync(
+        [FromQuery] TelemetryDataFilterDto filter,
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 10,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await service.GetAllDataAsync(filter, skip, take, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}", Name = "GetTelemetryDataById")]
+    public async Task<ActionResult<TelemetryDataResponse>> GetDataById(
+        Guid id,
+        CancellationToken cancellationToken)
+    {
+        var result = await service.GetDataByIdAsync(id, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddTelemetryDataAsync(
+        [FromBody] TelemetryDataRequest request,
+        CancellationToken cancellationToken)
+    {
+        var id = await service.AddDataAsync(request, cancellationToken);
+
+        var createdData = await service.GetDataByIdAsync(id, cancellationToken);
+
+        return CreatedAtRoute(
+            "GetTelemetryDataById",
+            new { id = id },
+            createdData);
+    }
+}
