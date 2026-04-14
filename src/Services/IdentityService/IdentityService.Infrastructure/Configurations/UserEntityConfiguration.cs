3232098e0dc601ed997324edfb280497eef17a21
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-04-14 10:01:17 +0300

    feat(identity/infrastructure) implement infrastructure layer

---
+++ src/Services/IdentityService/IdentityService.Infrastructure/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using IdentityService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityService.Infrastructure.Configurations;
+
+public class UserEntityConfiguration 
+    : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.SubscriptionEndDate).IsRequired();
+        builder.Property(x => x.SubscriptionId).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+
+        builder.HasOne<SubscriptionEntity>()
+            .WithMany()
+            .HasForeignKey(x => x.SubscriptionId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ src/Services/IdentityService/IdentityService.Infrastructure/IdentityDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IdentityService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Infrastructure;
+
+public class IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+    : IdentityDbContext<UserEntity, IdentityRole<Guid>, Guid>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+
+        foreach (var entity in builder.Model.GetEntityTypes())
+        {
+            var tableName = entity.GetTableName();
+            if (tableName != null && tableName.StartsWith("AspNet"))
+            {
+                entity.SetTableName(tableName.Substring(6).ToLower());
+            }
+        }
+    }
+}
